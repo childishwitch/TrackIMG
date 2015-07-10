8a44b08b922ac19352474b84bcd54f3035444f47
childishwitch <childishwitch@gmail.com>
2015-07-10 22:59:13 +0800

    delete some useless code

--- myApp/platforms/android/src/com/ionicframework/myapp235890/MainActivity.java
+++ myApp/platforms/android/src/com/ionicframework/myapp235890/MainActivity.java
@@ -24,76 +24,36 @@ import java.util.Date;
 
 public class MainActivity extends CordovaActivity{
 
-    private static int TAKE_PICTURE = 1;
-    private Uri imageUri;
-
+	private static boolean waiting = false;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera);
-
-        //自訂照片名稱 開始
-        Date curDateTime = new Date();
-        SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy-MM-dd HH.mm.ss");
-        String fileName = "checking_" + sdFormat.format(curDateTime) + ".jpg";
-        //自訂照面名稱 結束
-
-        //自訂檔案位置 開始
-        String root = Environment.getExternalStorageDirectory().toString();
-        File mydir = new File(root + "/checking/image/");
-        mydir.mkdirs();
-        //自訂檔案位置 結束
-
-        //啟動內建相機 並回傳圖片uri
-        Intent intent_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File photo = new File(mydir, fileName);
-        intent_camera.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(photo));
-        imageUri = Uri.fromFile(photo);
-        startActivityForResult(intent_camera, TAKE_PICTURE);
+		
+		if(!waiting){
+			//call the camera
+			Intent intent_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			startActivityForResult(intent_camera, 0);
+			waiting = true;
+		}
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        //super.onActivityResult(requestCode, resultCode, data);
-
-        //拍照後顯示圖片
+		waiting = false;
         if(resultCode == RESULT_OK)
         {
-            Uri selectedImage = imageUri;
-            getContentResolver().notifyChange(selectedImage,null);
             ImageView iv = (ImageView)findViewById(R.id.imgcaptured);
-            ContentResolver cr = getContentResolver();
-            Bitmap bitmap;
-            try{
-                bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr,selectedImage);
-                iv.setImageBitmap(bitmap);
-                Toast.makeText(this, selectedImage.toString(),Toast.LENGTH_LONG).show();
-            } catch (Exception e) {
-                Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
-                Log.e("Camera", e.toString());
-            }
+			Bundle extras = data.getExtras();
+			//transfer data into a bitmap format
+			Bitmap bitmap = (Bitmap)extras.get("data");
+			//show the image in the imageview
+			iv.setImageBitmap(bitmap);
         }
+		else
+		{	//cancel
+		}
+		super.onActivityResult(requestCode, resultCode, data);
     }
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
 }
