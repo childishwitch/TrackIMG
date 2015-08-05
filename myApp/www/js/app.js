bae05e1246a867805e7a7b6afe6dcc27d45ed4cd
childishwitch <childishwitch@gmail.com>
2015-08-05 12:17:00 +0800

    temp:floatingbutton action with orientation change

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -4,14 +4,6 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-/*window.onorientationchange = function(){
-	var orientation = window.orientation;
-	if(orientation == 90 || orientation == -90)
-		window.parent.location = "index_land.html";
-	else
-		window.parent.location = "index.html";
-	window.location.reload();
-};*/
 angular.module('starter', ['ionic', 'ng-mfb'])
 .controller("ContentCtrl", function($scope, $element, $window) {
 	/*
@@ -34,35 +26,61 @@ angular.module('starter', ['ionic', 'ng-mfb'])
             // An error occured. Show a message to the user
         });
     }*/
+/*	
+	$scope.getWindowOrientation = function () {
+		return $window.orientation;
+	}
+ 
+	$scope.$watch($scope.getWindowOrientation, function (newValue, oldValue) {
+		//if(newValue != oldValue){
+			if(newValue == 0)
+				parent.location = "index.html";
+			else if(newValue == 90 || newValue == -90) 
+				parent.location = "index_land.html";
+		//}
+	},true);*/
 	
 	$scope.myItems = ["台北","101大樓"];
 	$scope.sysItems = ["臺灣","煙火","跨年","台北市"];
-
-	$scope.getWindowOrientation = function () {
-		return $window.orientation;
-	};
-  
-	$scope.$watch($scope.getWindowOrientation, function (newValue, oldValue) {
-		if(newValue == 0)
-			parent.location = "index.html";
-		else
-			parent.location = "index_land.html";
-	}, true);
 	
-	
+	/*
 	//$window.onorientationchange = function () {
-	angular.element($window).bind('orientationchange', function () {
-		/*	
+	angular.element($window).on('orientationchange', function ($window,$scope) {
+			
+			//$scope.$apply(function($window,$scope){
+				if($window.orientation == 0)
+					$window.parent.location = "index.html";
+				else if($window.orientation == 90 || $window.orientation == -90)
+					$window.parent.location = "index_land.html";
+				//$window.location.reload();
+				$scope.degrees = $window.orientation;
+			//});
+	//}
+	});*/
+});
+/*
+function onorientationchange(){
+	var orientation = window.orientation;
+	if(orientation == 90 || orientation == -90)
+		parent.location = "index_land.html";
+	else
+		parent.location = "index.html";
+	//window.location.reload();
+}
+window.addEventListenter('orientationchange',onorientationchange);
+onorientationchange();*/
+/*
+	//$window.onorientationchange = function () {
+.element($window).bind('orientationchange', function ($window,$scope) {
+			//$window.location.reload();
 			if($window.orientation == 0)
 				parent.location = "index.html";
 			else
-				parent.location = "index_land.html";*/
-			//$scope.degrees = 100;
-			//$window.location.reload();
+				parent.location = "index_land.html";
 			$scope.$apply();
 	//};
-	});
-});
+});*/
+	
 /*
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
