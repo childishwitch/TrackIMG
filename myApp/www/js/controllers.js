da6f0e10e957c67cdcdeb429bd8961cba49778ef
childishwitch <childishwitch@gmail.com>
2015-08-27 02:22:53 +0800

    update orientation when initiation

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -8,29 +8,34 @@
       $ionicSideMenuDelegate.toggleLeft($scope.$$childHead);
     }
   }];*/
-	
+
+  	  var updateOrientation=function(){
+			if(window.orientation == 0)
+				parent.location = "#/event/port";
+			else if(window.orientation == 90 || window.orientation == -90)
+				parent.location = "#/event/land";
+			//window.location.reload();
+               /*var body=document.body;  
+               var viewport=document.getElementById("viewport");  
+               if(body.getAttribute("orient")=="landscape"){  
+                   body.setAttribute("orient","portrait");  
+               }else{  
+                   body.setAttribute("orient","landscape");  
+               }*/  
+   };
+
+  var init=function(){  
+      updateOrientation();  
+      window.addEventListener("orientationchange",updateOrientation,false);  
+  };  
+  window.addEventListener("DOMContentLoaded",init,false);
+  
   $scope.myItems = ["台北","101大樓"];
 	$scope.sysItems = ["臺灣","煙火","跨年","台北市"];  
 })
 
 .controller("ContentCtrl", function($scope, $element, $window) {
-		window.onorientationchange=function(){
-				if(window.orientation == 0)
-					parent.location = "#/event/port";
-				else if(window.orientation == 90 || window.orientation == -90)
-					parent.location = "#/event/land";
-				window.location.reload();
-                /*var body=document.body;  
-                var viewport=document.getElementById("viewport");  
-                if(body.getAttribute("orient")=="landscape"){  
-                    body.setAttribute("orient","portrait");  
-                }else{  
-                    body.setAttribute("orient","landscape");  
-                }*/  
-        };  
-		$scope.getWindowOrientation = function () {
-		return $window.orientation;
-		}
+
 })
 
 .controller('LandCtrl', function($scope) {
